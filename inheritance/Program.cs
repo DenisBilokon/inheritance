3b1183a2dbae54ca5e9fedceba17c46795756cd9
DenisBilokon <Superden994@gmail.com>
2023-09-11 18:32:43 +0300

    Добавьте файлы проекта.

---
+++ inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Animal
+{
+    public string Species { get; set; }
+    public int Age { get; set; }
+
+    public Animal(string species, int age)
+    {
+        Species = species;
+        Age = age;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Вид: {Species}, Возраст: {Age} лет");
+    }
+}
+
+// Лев - зверь, наследуеться от класа "Зверь".
+public class Lion : Animal
+{
+    public Lion(int age) : base("Лев", age)
+    {
+    }
+}
+
+
+public class Tiger : Animal
+{
+    public Tiger(int age) : base("Тигр", age)
+    {
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal lion = new Lion(8);
+        Animal tiger = new Tiger(6);
+
+        lion.DisplayInfo();  
+        tiger.DisplayInfo(); 
+    }
+}
